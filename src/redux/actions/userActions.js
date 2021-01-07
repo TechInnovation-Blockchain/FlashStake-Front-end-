f4a99d9332c7428a082b06b9e887240c9398c555
TechInnovation-Blockchain <newcaesar628@gmail.com>
2021-01-07 21:19:00 +0500

    apy for less than 18 dec

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -121,7 +121,8 @@ export const updateApyPools = (quantity, poolsParam) => async (
           JSBI.BigInt("1000"),
           JSBI.divide(_fpy0, JSBI.BigInt(5e15))
         );
-        const _apyStake = Web3.utils.fromWei(
+
+        const _apyStake = utils.formatUnits(
           String(
             JSBI.divide(
               JSBI.multiply(
@@ -136,7 +137,8 @@ export const updateApyPools = (quantity, poolsParam) => async (
                 JSBI.multiply(_fpy, _lpFee)
               )
             )
-          )
+          ),
+          _pools[i]?.tokenB?.decimal || _pools[i]?.tokenB?.decimals
         );
         const tokenPrice = response.data[_pools[i].tokenB.id].usd || 0;
         // const _apyStake = await _getAPYStake(_pools[i].id, _fpy);
